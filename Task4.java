861772f7dea51a95193470d1f05a4e8727ab5e3d
Svetlana-18 <vasilevichsvetlana@yandex.ru>
2022-12-27 19:54:03 +0300

    отформатировала

--- Task4.java
+++ Task4.java
@@ -3,19 +3,19 @@ public class Task4 {
     public String reverseWords(String s) {
         StringBuilder sb = new StringBuilder();
         int end = s.length() - 1;
-        while (end >= 0){
+        while (end >= 0) {
             while (end > 0 && s.charAt(end) == ' ') {
-                end-=1;
-            } 
-            int start= end - 1;
-        while (start >= 0 && s.charAt(start) !=' ') {
-                start-=1;
+                end -= 1;
+            }
+            int start = end - 1;
+            while (start >= 0 && s.charAt(start) != ' ') {
+                start -= 1;
             }
             sb.append(' ');
             sb.append(s.substring(start + 1, end + 1));
             end = start - 1;
         }
-        if(sb.length()>0){
+        if (sb.length() > 0) {
             sb.deleteCharAt(0);
         }
         return sb.toString();
--- Task5.java
+++ Task5.java
@@ -4,18 +4,22 @@ public class Task5 {
         public boolean isPalindrome(String s) {
             int left = 0;
             int right = s.length() - 1;
-    
+
             while (left < right) {
-                while (left < right && ! Character.isLetterOrDigit(s.charAt(left))); {
-                    left +=1;
+                while (left < right && !Character.isLetterOrDigit(s.charAt(left)))
+                    ;
+                {
+                    left += 1;
                 }
-                while (left < right && ! Character.isLetterOrDigit(s.charAt(right))); {
-                    right -=1;
+                while (left < right && !Character.isLetterOrDigit(s.charAt(right)))
+                    ;
+                {
+                    right -= 1;
                 }
                 char leftChar = Character.toLowerCase(s.charAt(left));
                 char rightChar = Character.toLowerCase(s.charAt(right));
-    
-                if (leftChar != rightChar){
+
+                if (leftChar != rightChar) {
                     return false;
                 }
                 left += 1;
